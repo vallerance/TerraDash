5cdc6dee237e15a4ba9539f622341082621e0303
phaeax-a01 <phaeax.a01@gmail.com>
2026-08-20 18:01:21 +0000

    Trace custom quiz browser startup

--- browser/quiz-selector.spec.ts
+++ browser/quiz-selector.spec.ts
@@ -51,6 +51,7 @@ test('header navbar exposes all quizzes and enters the selected quiz', async ({
 });
 
 test('selects and starts the non-UN quiz', async ({ page }) => {
+  page.on('pageerror', (error) => console.log(`PAGEERROR ${error.stack}`));
   await page.goto('/TerraDash/');
   const title = nonUnTitle;
   await page.getByRole('button', { name: title }).click();
